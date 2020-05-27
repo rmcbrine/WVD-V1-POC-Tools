abb5ba9d133d654f6ddc767354094a1c2cfa04ba
rmcbrine <rmcbrine@microsoft.com>
2020-05-27 12:51:07 -0400

    Added a few scripts

---
+++ Create Svc Principal.ps1
@@ -0,0 +1,19 @@
+﻿
+Install-Module AzureAD
+
+Import-Module AzureAD
+$aadContext = Connect-AzureAD
+$svcPrincipal = New-AzureADApplication -AvailableToOtherTenants $true -DisplayName "Windows Virtual Desktop Svc Principal"
+$svcPrincipalCreds = New-AzureADApplicationPasswordCredential -ObjectId $svcPrincipal.ObjectId
+
+#Output Password for Service Principal:
+$svcPrincipalCreds.Value
+
+#Output AAD Tenant GUID for Service Principal:
+$aadContext.TenantId.Guid
+
+#Output App ID for Service Principal:
+$svcPrincipal.AppId
+
+$myTenantName = "East2-WVD-V1-PRD"
+New-RdsRoleAssignment -RoleDefinitionName "RDS Owner" -ApplicationId $svcPrincipal.AppId -TenantName $myTenantName
---
+++ Manual Steps Host Pool to Reg.ps1
@@ -0,0 +1,25 @@
+﻿
+
+$SubGUID = "698f872a-dddf-403c-b04e-fd432b405cb5"
+$SubGUID
+$AADGUID = "26323443-7c91-49e0-bdc2-a4ceac7d7dbb"
+$AADGUID 
+$WVDTenantName = "East2-WVD-V1-PRD"
+$WVDTenantName
+$HostPoolNameDesktops = "WVD-V1-East2-DeskHostPool"
+$HostPoolNameDesktops
+$HostPoolNameApps = "WVD-V1-East2-AppHostPool"
+$HostPoolNameApps
+$AppGroupName = "WVDV1-East2-AppGroup"
+$AppGroupName
+$ResourceGroupName = "East2-PRD-WVDv1"
+
+New-RdsHostPool -TenantName $WVDTenantName -Name $HostPoolNameDesktops
+
+New-RdsRegistrationInfo -TenantName $WVDTenantName -HostPoolName $HostPoolNameDesktops -ExpirationHours 48 
+
+Export-RdsRegistrationInfo -TenantName $WVDTenantName -HostPoolName $HostPoolNameDesktops | Select-Object -ExpandProperty Token > c:\scratch\registrationtoken2.txt
+
+https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv
+
+https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH
